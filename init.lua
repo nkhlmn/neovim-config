905d93023a08c25734d21fce4098959d4158b6c2
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2021-08-17 00:49:52 -0700

    Move some more stuff to lua

--- init.lua
+++ init.lua
@@ -2,31 +2,28 @@ require('config.plugins')
 require('config.colorscheme')
 require('config.treesitter')
 require('config.lspconfig')
+require('config.toggleterm')
 
 local vim = vim
+local api = vim.api
 local o = vim.o
-local wo = vim.wo
-local bo = vim.bo
+local g = vim.g
+-- local wo = vim.wo
+-- local bo = vim.bo
 
 
 vim.cmd[[
 source ~/.config/nvim/lightline.vim
 source ~/.config/nvim/custom_functions.vim
 source ~/.config/nvim/autocmd.vim
-source ~/.config/nvim/remap.vim
 source ~/.config/nvim/telescope.vim
 source ~/.config/nvim/nvim-compe.vim
 source ~/.config/nvim/lspsaga.vim
-
-syntax on
-filetype plugin on
 ]]
 
 o.completeopt = 'menuone,noselect'
 o.number = true
 o.relativenumber = true
-
-
 o.conceallevel = 0
 o.expandtab = true
 o.shiftwidth = 2
@@ -46,12 +43,38 @@ o.ignorecase = true -- Make search case insensitive
 o.hlsearch = true -- make search highlight all matches
 o.incsearch = true -- start search immediately after typing
 o.diffopt = 'internal,filler,closeoff,vertical'
--- nnoremap * *N
-
+api.nvim_set_keymap('n', '*', '*N', {})
 o.foldlevelstart = 99
 
 
 -- NETRW
--- o.netrw_liststyle = 3
+g.netrw_liststyle = 3
 o.splitbelow = true
 o.splitright = true
+
+
+-- Mappings
+api.nvim_set_keymap('', '<C-S>', ':w<CR>', {})
+api.nvim_set_keymap('', 'Q', '<Nop>', {})
+api.nvim_set_keymap('', '<leader>n', ':set number!<CR>', {})
+api.nvim_set_keymap('n', '<leader>y', ':%y<CR>', {})
+api.nvim_set_keymap('n', '<ESC><ESC>', ':let @/ = ""<cr>', { noremap = true, silent = true })
+
+api.nvim_set_keymap('n', '<leader>t', ':tabnew<CR>', { silent = true })
+api.nvim_set_keymap('n', '<leader>T', ':Texplore<CR>', { silent = true })
+api.nvim_set_keymap('n', '<leader>s', ':new<CR>', { silent = true })
+api.nvim_set_keymap('n', '<leader>S', ':Sexplore<CR>', { silent = true })
+api.nvim_set_keymap('n', '<leader>v', ':vnew<CR>', { silent = true })
+api.nvim_set_keymap('n', '<leader>V', ':vnew<bar>Explore<CR>', { silent = true })
+
+
+api.nvim_set_keymap('n', '[t', ':tabprevious<CR>', { silent = true, noremap = true })
+api.nvim_set_keymap('n', ']t', ':tabnext<CR>', { silent = true, noremap = true })
+api.nvim_set_keymap('n', '[T', ':execute "silent! tabmove " . (tabpagenr()-2)<CR>', { silent = true, noremap = true })
+api.nvim_set_keymap('n', ']T', ':execute "silent! tabmove " . (tabpagenr()+1)<CR>', { silent = true, noremap = true })
+
+api.nvim_set_keymap('', '<leader>`', ':tabnew $MYVIMRC<CR>', { noremap = true, silent = true })
+
+api.nvim_set_keymap('n', ' ', 'za', { noremap = true, silent = true })
+
+g.user_emmet_leader_key = '<C-E>'
--- telescope.vim
+++ telescope.vim
@@ -2,7 +2,7 @@
 nnoremap <leader>ff <cmd>Telescope find_files<cr>
 nnoremap <leader>fg <cmd>Telescope live_grep<cr>
 nnoremap <leader>fb <cmd>Telescope buffers<cr>
-nnoremap <leader>fh <cmd>Telescope help_tags<cr>
+nnoremap <leader>h <cmd>Telescope help_tags<cr>
 nnoremap <leader>b <cmd>Telescope file_browser<cr>
 
 nnoremap <C-p> <cmd>Telescope find_files<cr>
