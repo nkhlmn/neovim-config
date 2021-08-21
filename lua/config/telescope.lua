e82a343cae19cf683f9ec9e21c267f22af41eed1
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2021-08-20 23:19:04 -0700

    Add more telescope keymappings

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -4,8 +4,10 @@ local api = vim.api
 api.nvim_set_keymap('n', '<leader>ff', ':Telescope find_files<cr>', { noremap = true })
 api.nvim_set_keymap('n', '<leader>fg', ':Telescope live_grep<cr>', { noremap = true })
 api.nvim_set_keymap('n', '<leader>fb', ':Telescope buffers<cr>', { noremap = true })
-api.nvim_set_keymap('n', '<leader>h', ':Telescope help_tags<cr>', { noremap = true })
+api.nvim_set_keymap('n', '<leader>h', ':Telescope highlights<cr>', { noremap = true })
+api.nvim_set_keymap('n', '<leader>k', ':Telescope keymaps<cr>', { noremap = true })
 api.nvim_set_keymap('n', '<C-b>', ':Telescope file_browser<cr>', { noremap = true })
 api.nvim_set_keymap('n', '<C-p>', ':Telescope find_files<cr>', { noremap = true })
 api.nvim_set_keymap('n', '<C-f>', ':Telescope live_grep<cr>', { noremap = true })
-
+api.nvim_set_keymap('n', '<C-q>', ':Telescope quickfix<cr>', { noremap = true })
+api.nvim_set_keymap('n', '<C-h>', ':Telescope help_tags<cr>', { noremap = true })
