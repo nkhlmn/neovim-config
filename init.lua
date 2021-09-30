802b63cea8971058e757a138866a99cb7f4a8f47
nkhl <nkhl@pop-os.localdomain>
2021-09-30 00:11:26 -0700

    Move remaining vimscript config to lua

--- init.lua
+++ init.lua
@@ -15,13 +15,6 @@ local g = vim.g
 -- local wo = vim.wo
 -- local bo = vim.bo
 
-
-vim.cmd[[
-source ~/.config/nvim/lightline.vim
-source ~/.config/nvim/custom_functions.vim
-source ~/.config/nvim/autocmd.vim
-]]
-
 o.completeopt = 'menuone,noselect'
 o.number = true
 o.relativenumber = true
@@ -44,7 +37,6 @@ o.ignorecase = true -- Make search case insensitive
 o.hlsearch = true -- make search highlight all matches
 o.incsearch = true -- start search immediately after typing
 o.diffopt = 'internal,filler,closeoff,vertical'
-api.nvim_set_keymap('n', '*', '*N', {})
 o.foldlevelstart = 99
 
 
@@ -56,27 +48,103 @@ o.splitright = true
 
 -- Mappings
 vim.g.mapleader = ' '
-api.nvim_set_keymap('', '<C-S>', ':w<CR>', {})
-api.nvim_set_keymap('', 'Q', '<Nop>', {})
-api.nvim_set_keymap('', '<leader>n', ':set number!<CR>', {})
-api.nvim_set_keymap('n', '<leader>y', ':%y<CR>', {})
-api.nvim_set_keymap('n', '<ESC><ESC>', ':let @/ = ""<cr>', { noremap = true, silent = true })
+api.nvim_set_keymap('n', '*', '*N', {}) -- keep cursor on first match when searching for word under cursor
+api.nvim_set_keymap('', '<C-S>', ':w<CR>', {}) -- save buffer
+api.nvim_set_keymap('n', 'Q', '<Nop>', {}) -- prevent going into ex mode
+-- api.nvim_set_keymap('', '<leader>n', ':set number!<CR>', {})
+api.nvim_set_keymap('n', '<leader>y', ':%y<CR>', {}) -- yank entire buffer
+api.nvim_set_keymap('n', '<ESC><ESC>', ':let @/ = ""<cr>', { noremap = true, silent = true }) -- clear search by pressing esc twice
 
-api.nvim_set_keymap('n', '<leader>t', ':tabnew<CR>', { silent = true })
-api.nvim_set_keymap('n', '<leader>T', ':Texplore<CR>', { silent = true })
-api.nvim_set_keymap('n', '<leader>s', ':new<CR>', { silent = true })
-api.nvim_set_keymap('n', '<leader>S', ':Sexplore<CR>', { silent = true })
-api.nvim_set_keymap('n', '<leader>v', ':vnew<CR>', { silent = true })
-api.nvim_set_keymap('n', '<leader>V', ':vnew<bar>Explore<CR>', { silent = true })
+api.nvim_set_keymap('n', '<leader>t', ':tabnew<CR>', { silent = true }) -- open new tab
+api.nvim_set_keymap('n', '<leader>T', ':Texplore<CR>', { silent = true }) -- open new tab with netrw
+api.nvim_set_keymap('n', '<leader>s', ':new<CR>', { silent = true }) -- open horizontal split
+api.nvim_set_keymap('n', '<leader>S', ':Sexplore<CR>', { silent = true }) -- open horizontal split with netrw
+api.nvim_set_keymap('n', '<leader>v', ':vnew<CR>', { silent = true }) -- open vertical split
+api.nvim_set_keymap('n', '<leader>V', ':vnew<bar>Explore<CR>', { silent = true }) -- open vertical split with netrw
 
 
-api.nvim_set_keymap('n', '[t', ':tabprevious<CR>', { silent = true, noremap = true })
-api.nvim_set_keymap('n', ']t', ':tabnext<CR>', { silent = true, noremap = true })
-api.nvim_set_keymap('n', '[T', ':execute "silent! tabmove " . (tabpagenr()-2)<CR>', { silent = true, noremap = true })
-api.nvim_set_keymap('n', ']T', ':execute "silent! tabmove " . (tabpagenr()+1)<CR>', { silent = true, noremap = true })
+api.nvim_set_keymap('n', '[t', ':tabprevious<CR>', { silent = true, noremap = true }) -- prev tab
+api.nvim_set_keymap('n', ']t', ':tabnext<CR>', { silent = true, noremap = true }) -- next tab
+api.nvim_set_keymap('n', '[T', ':execute "silent! tabmove " . (tabpagenr()-2)<CR>', { silent = true, noremap = true }) -- move tab left
+api.nvim_set_keymap('n', ']T', ':execute "silent! tabmove " . (tabpagenr()+1)<CR>', { silent = true, noremap = true }) -- move tab right
 
-api.nvim_set_keymap('', '<leader>`', ':tabnew $MYVIMRC<CR>', { noremap = true, silent = true })
+api.nvim_set_keymap('', '<leader>`', ':tabnew $MYVIMRC<CR>', { noremap = true, silent = true }) -- open init.lua
 
 api.nvim_set_keymap('n', ' ', 'za', { noremap = true, silent = true })
 
 g.user_emmet_leader_key = '<C-E>'
+
+-- Highlight on yank
+vim.cmd [[au TextYankPost * silent! lua vim.highlight.on_yank()]]
+
+-- Yank till end of line with Y (instead of yanking entire line)
+api.nvim_set_keymap('n', 'Y', 'y$', { noremap = true, silent = true })
+
+-- Leave cursor where it is after yanking
+api.nvim_set_keymap('n', 'y^', 'mzy^`z', { noremap = true, silent = true })
+api.nvim_set_keymap('n', 'ygg', "mzygg`z", { noremap = true, silent = true })
+
+-- Keep things centered when jumping and joining lines ('zz' centers cursor, 'zv' opens folds)
+api.nvim_set_keymap('n', 'n', 'nzzzv', { noremap = true, silent = true })
+api.nvim_set_keymap('n', 'N', 'Nzzzv', { noremap = true, silent = true })
+api.nvim_set_keymap('n', 'J', 'mzJ`z', { noremap = true, silent = true })
+
+-- Create undo break points when hitting certain characters in insert mode (allows for more granular undos)
+api.nvim_set_keymap('i', ',', ',<c-g>u', { noremap = true, silent = true })
+api.nvim_set_keymap('i', '.', '.<c-g>u', { noremap = true, silent = true })
+api.nvim_set_keymap('i', '!', '!<c-g>u', { noremap = true, silent = true })
+api.nvim_set_keymap('i', '?', '?<c-g>u', { noremap = true, silent = true })
+
+-- Open current file with default program
+api.nvim_set_keymap('n', '<F5>', ':!open %<CR>', { silent = true })
+
+-- Paste last thing yanked
+api.nvim_set_keymap('n', ',p', '"0p', { silent = true })
+api.nvim_set_keymap('n', ',P', '"0P', { silent = true })
+
+-- Toggle diff
+function _G.toggle_diff()
+  local action = (vim.o.diff == true and 'off' or 'this')
+  vim.cmd('windo diff' .. action)
+end
+
+api.nvim_set_keymap('n', '<leader>d', '<cmd>lua toggle_diff()<cr>', {})
+
+-- Show highlight group under cursor
+vim.cmd[[
+map <F3> :echo "hi<" . synIDattr(synID(line("."),col("."),1),"name") . '> trans<' . synIDattr(synID(line("."),col("."),0),"name") . "> lo<" . synIDattr(synIDtrans(synID(line("."),col("."),1)),"name") . ">" . " FG:" . synIDattr(synIDtrans(synID(line("."),col("."),1)),"fg#")<CR>
+]]
+
+-- Toggle whitespace
+vim.cmd[[
+" https://stackoverflow.com/questions/9104706/how-can-i-convert-spaces-to-tabs-in-vim-or-linux
+function! ToggleWhitespace()
+  setlocal lcs=tab:>-,trail:-,eol:$ list! list?
+  if &list
+    match ErrorMsg /\s\+$/
+  else
+    match none
+  endif
+endfunction
+
+nnoremap <F2> :call ToggleWhitespace()<CR>
+]]
+
+-- Parse URL
+vim.cmd[[
+function! ParseURL()
+  execute "%s/#/\r#\r/ge"
+  execute "%s/?/\r?\r/ge"
+  execute "%s/&/\r&\r/ge"
+  execute "%s/%5B/[/ge"
+  execute "%s/%5D/]/ge"
+  execute "%s!%2F!/!ge"
+endfunction
+]]
+
+vim.cmd[[
+" Trigger `autoread` when files changes on disk
+autocmd FocusGained,BufEnter,CursorHold,CursorHoldI * if !bufexists("[Command Line]") | checktime | endif
+" Notification after file change
+autocmd FileChangedShellPost * echohl WarningMsg | echo "File changed on disk. Buffer reloaded." | echohl None
+]]
--- lua/config/colorscheme.lua
+++ lua/config/colorscheme.lua
@@ -1,14 +1,91 @@
 local vim = vim
-local o = vim.o
+local o = vim.opt
 local g = vim.g
 
+-- COLORSCHEME
+-- General options
 o.termguicolors = true
+o.background = 'dark'
 
--- gruxbox_material settings
-g.gruvbox_material_background = 'hard'
+ -- Specify colorscheme name here
+-- local colorscheme_name = 'spacegray'
+-- local colorscheme_name = 'everforest'
+local colorscheme_name = 'everforest'
+-- local colorscheme_name = 'sonokai'
+-- local colorscheme_name = 'edge'
 
--- everforest settings
-g.everforest_background = 'hard'
+-- Theme specific config options
+if(colorscheme_name == 'gruvbox-material') then
+	g.gruvbox_material_background = 'hard'
+	g.gruvbox_material_sign_column_background = 'none'
+	g.gruvbox_material_diagnostic_text_highlight = true
+	g.gruvbox_material_diagnostic_line_highlight = true
+	g.gruvbox_material_diagnostic_virtual_text = 'colored'
+  -- g.gruvbox_material_disable_italic_comment = true
+elseif(colorscheme_name == 'everforest') then
+	g.everforest_background = 'hard'
+	g.everforest_sign_column_background = 'none'
+	g.everforest_diagnostic_text_highlight = true
+	g.everforest_diagnostic_line_highlight = true
+	g.everforest_diagnostic_virtual_text = 'colored'
+	-- g.everforest_disable_italic_comment = true
+elseif(colorscheme_name == 'edge') then
+	g.edge_disable_italic_comment = true
+end
 
--- vim.cmd[[colorscheme gruvbox-material]]
-vim.cmd[[colorscheme everforest]]
+-- Set the colorscheme
+vim.cmd('colorscheme ' .. colorscheme_name)
+
+-- LIGHTLINE
+local lightline_theme_mapping = {
+	['ayu'] = 'ayu',
+	['gruvbox-material'] = 'gruvbox_material',
+	['sonokai'] = 'sonokai',
+	['everforest'] = 'everforest',
+	['spacegray'] = 'darcula',
+	['onedark'] = 'onedark',
+	['edge'] = 'edge',
+}
+
+vim.cmd[[
+function! GetActiveLspClients()
+	try
+		return luaeval('require("config.lsp").get_attached_lsp_servers()')
+	catch
+		return ''
+	endtry
+endfunction
+
+function! GetGitHead()
+	try
+		return b:gitsigns_head
+	catch
+		return ''
+	endtry
+endfunction
+
+function! GetGitStatus()
+	try
+		return b:gitsigns_status
+	catch
+		return ''
+	endtry
+endfunction
+]]
+
+g.lightline = {
+	colorscheme = lightline_theme_mapping[g.colors_name],
+	active = {
+		left = {
+			{ 'mode', 'paste' },
+			{ 'gitbranch', 'gitstatus' },
+			{ 'readonly', 'filename', 'modified', 'active_lsp_clients' },
+		}
+	},
+	component_function = {
+		gitbranch = 'GetGitHead',
+		gitstatus = 'GetGitStatus',
+		active_lsp_clients = 'GetActiveLspClients',
+	},
+}
+
