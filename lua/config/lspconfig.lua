07891a59ff2321316bbd55abf527ec1c88ddef6d
nkhl <nkhl@pop-os.localdomain>
2021-10-31 14:49:38 -0700

    Update mappings for lsp

--- lua/config/lspconfig.lua
+++ lua/config/lspconfig.lua
@@ -5,6 +5,7 @@ local on_attach = function(client, bufnr)
   local opts = { noremap=true, silent=true }
   buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
   buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  buf_set_keymap('n', '<leader>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
 end
 
 local function setup_servers()
--- lua/config/lspsaga.lua
+++ lua/config/lspsaga.lua
@@ -10,11 +10,11 @@ saga.init_lsp_saga {
   border_style = "round",
 }
 
-api.nvim_set_keymap('n', 'K', ':Lspsaga hover_doc<cr>', { silent = true, noremap = true })
-api.nvim_set_keymap('n', 'gs', ':Lspsaga signature_help<cr>', { silent = true, noremap = true })
-api.nvim_set_keymap('n', 'gh', ':Lspsaga lsp_finder<cr>', { silent = true, noremap = true })
-api.nvim_set_keymap('n', 'gd', ':Lspsaga preview_definition<cr>', { silent = true, noremap = true })
-api.nvim_set_keymap('n', 'gr', ':Lspsaga rename<cr>', { silent = true, noremap = true })
+api.nvim_set_keymap('n', '<leader>gh', ':Lspsaga hover_doc<cr>', { silent = true, noremap = true })
+api.nvim_set_keymap('n', '<leader>gs', ':Lspsaga signature_help<cr>', { silent = true, noremap = true })
+api.nvim_set_keymap('n', '<leader>gf', ':Lspsaga lsp_finder<cr>', { silent = true, noremap = true })
+api.nvim_set_keymap('n', '<leader>gd', ':Lspsaga preview_definition<cr>', { silent = true, noremap = true })
+api.nvim_set_keymap('n', '<leader>rn', ':Lspsaga rename<cr>', { silent = true, noremap = true })
 api.nvim_set_keymap('n', '<leader>cd', ':Lspsaga show_line_diagnostics<cr>', { silent = true, noremap = true })
 api.nvim_set_keymap('n', '<leader>cc', ':Lspsaga show_cursor_diagnostics<cr>', { silent = true, noremap = true })
 api.nvim_set_keymap('n', '[e', ':Lspsaga diagnostic_jump_prev<cr>', { silent = true, noremap = true })
