0ba7f39548f40ab60433a693f0b9e17f879f148b
nkhlmn <n/a>
2021-12-27 02:49:52 -0800

    Update mappings

--- lua/config/lspsaga.lua
+++ lua/config/lspsaga.lua
@@ -1,7 +1,6 @@
-local vim = vim
-local api = vim.api
+local map = vim.api.nvim_set_keymap
 
-local saga = require 'lspsaga'
+local saga = require'lspsaga'
 saga.init_lsp_saga {
   error_sign = '',
   warn_sign = '',
@@ -10,12 +9,12 @@ saga.init_lsp_saga {
   border_style = "round",
 }
 
-api.nvim_set_keymap('n', '<leader>gh', ':Lspsaga hover_doc<cr>', { silent = true, noremap = true })
-api.nvim_set_keymap('n', '<leader>gs', ':Lspsaga signature_help<cr>', { silent = true, noremap = true })
-api.nvim_set_keymap('n', '<leader>gf', ':Lspsaga lsp_finder<cr>', { silent = true, noremap = true })
-api.nvim_set_keymap('n', '<leader>gd', ':Lspsaga preview_definition<cr>', { silent = true, noremap = true })
-api.nvim_set_keymap('n', '<leader>rn', ':Lspsaga rename<cr>', { silent = true, noremap = true })
-api.nvim_set_keymap('n', '<leader>cd', ':Lspsaga show_line_diagnostics<cr>', { silent = true, noremap = true })
-api.nvim_set_keymap('n', '<leader>cc', ':Lspsaga show_cursor_diagnostics<cr>', { silent = true, noremap = true })
-api.nvim_set_keymap('n', '[e', ':Lspsaga diagnostic_jump_prev<cr>', { silent = true, noremap = true })
-api.nvim_set_keymap('n', ']e', ':Lspsaga diagnostic_jump_next<cr>', { silent = true, noremap = true })
+map('n', '<leader>gh', ':Lspsaga hover_doc<cr>', { silent = true, noremap = true })
+map('n', '<leader>gs', ':Lspsaga signature_help<cr>', { silent = true, noremap = true })
+map('n', '<leader>gf', ':Lspsaga lsp_finder<cr>', { silent = true, noremap = true })
+map('n', '<leader>gd', ':Lspsaga preview_definition<cr>', { silent = true, noremap = true })
+map('n', '<leader>rn', ':Lspsaga rename<cr>', { silent = true, noremap = true })
+map('n', '<leader>cd', ':Lspsaga show_line_diagnostics<cr>', { silent = true, noremap = true })
+map('n', '<leader>cc', ':Lspsaga show_cursor_diagnostics<cr>', { silent = true, noremap = true })
+map('n', '[e', ':Lspsaga diagnostic_jump_prev<cr>', { silent = true, noremap = true })
+map('n', ']e', ':Lspsaga diagnostic_jump_next<cr>', { silent = true, noremap = true })
--- lua/config/trouble.lua
+++ lua/config/trouble.lua
@@ -1,1 +1,5 @@
+local map = vim.api.nvim_set_keymap
+
 require("trouble").setup{}
+
+map('n', '<leader>gt', ':TroubleToggle<cr>', { silent = true, noremap = true })
