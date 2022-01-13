de2a1b5bd0ac6228aa0d80cef452507c45ff4907
nkhlmn <n/a>
2022-01-12 23:12:52 -0800

    Update lspconfig

--- lua/config/lspconfig.lua
+++ lua/config/lspconfig.lua
@@ -1,11 +1,18 @@
 local vim = vim
 
 local on_attach = function(client, bufnr)
-  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end  -- Mappings.
+  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
   local opts = { noremap=true, silent=true }
   buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
   buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  buf_set_keymap('n', '<leader>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+  buf_set_keymap('n', '<leader>ca', '<Cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+  buf_set_keymap('n', '<leader>gh', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  buf_set_keymap('n', '<leader>gs', '<Cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  buf_set_keymap('n', '<leader>rn', '<Cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  -- buf_set_keymap('n', '<leader>f', '<Cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+  buf_set_keymap('n', '<leader>f', '<Cmd>Neoformat<CR>', opts)
+
+  require('notify')("Attached to " .. client.name)
 end
 
 local lsp_installer = require("nvim-lsp-installer")
@@ -13,17 +20,20 @@ local capabilities = require('cmp_nvim_lsp').update_capabilities(
 	vim.lsp.protocol.make_client_capabilities()
 )
 
--- Register a handler that will be called for all installed servers.
 lsp_installer.on_server_ready(function(server)
 	local opts = {
 		on_attach = on_attach,
 		capabilities = capabilities,
 	}
 
-	-- (optional) Customize the options passed to the server
 	if server.name == "rust_analyzer" then
-		require("rust-tools").setup({
-			server = { cmd = server._default_options.cmd },
+		require('rust-tools').setup({
+			tools = {
+				inlay_hints = { highlight = "DiagnosticSignHint" },
+			},
+			server = {
+				on_attach = on_attach,
+			},
 		})
 		return
 	elseif server.name == "sumneko_lua" then
@@ -36,7 +46,5 @@ lsp_installer.on_server_ready(function(server)
 		}
 	end
 
-	-- This setup() function is exactly the same as lspconfig's setup function.
-	-- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
-	server:setup(opts)
+	server:setup(opts) -- This is the same as lspconfig's setup function.
 end)
