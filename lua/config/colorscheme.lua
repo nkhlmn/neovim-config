ca0b4e6018dc951fcdfe93ac95832c8f62f88e1a
nkhlmn <n/a>
2022-01-22 13:28:38 -0800

    Support local.lua and local colorscheme option
    Allow for defining a non source controlled local.lua file that will
    allow for specifying config options specfic to an environment

--- lua/config/colorscheme.lua
+++ lua/config/colorscheme.lua
@@ -2,36 +2,37 @@ local vim = vim
 local o = vim.opt
 local g = vim.g
 
--- COLORSCHEME
 -- General options
 o.termguicolors = true
 o.background = 'dark'
 
- -- Specify colorscheme name here
--- local colorscheme_name = 'spacegray'
--- local colorscheme_name = 'everforest'
-local colorscheme_name = 'gruvbox-material'
--- local colorscheme_name = 'kanagawa'
+-- Check if a local config file (not source controlled) exists
+local has_local_config = pcall(require, 'config.local')
+if has_local_config then
+	local local_config = require('config.local').get_local_config()
+	local colorscheme_name = local_config.colorscheme
 
--- Theme specific config options
-if(colorscheme_name == 'gruvbox-material') then
-	g.gruvbox_material_background = 'hard'
-	g.gruvbox_material_sign_column_background = 'none'
-	g.gruvbox_material_diagnostic_text_highlight = true
-	g.gruvbox_material_diagnostic_line_highlight = true
-	g.gruvbox_material_diagnostic_virtual_text = 'colored'
-	g.gruvbox_material_enable_bold = true
-	g.gruvbox_material_better_performance = true
-  -- g.gruvbox_material_disable_italic_comment = true
-elseif(colorscheme_name == 'everforest') then
-	g.everforest_background = 'hard'
-	g.everforest_sign_column_background = 'none'
-	g.everforest_diagnostic_text_highlight = true
-	g.everforest_diagnostic_line_highlight = true
-	g.everforest_diagnostic_virtual_text = 'colored'
-	-- g.everforest_disable_italic_comment = true
-end
+	-- Theme specific config options
+	if(colorscheme_name == 'gruvbox-material') then
+		g.gruvbox_material_background = 'hard'
+		g.gruvbox_material_sign_column_background = 'none'
+		g.gruvbox_material_diagnostic_text_highlight = true
+		g.gruvbox_material_diagnostic_line_highlight = true
+		g.gruvbox_material_diagnostic_virtual_text = 'colored'
+		g.gruvbox_material_enable_bold = true
+		g.gruvbox_material_better_performance = true
+		-- g.gruvbox_material_disable_italic_comment = true
+	elseif(colorscheme_name == 'everforest') then
+		g.everforest_background = 'hard'
+		g.everforest_sign_column_background = 'none'
+		g.everforest_diagnostic_text_highlight = true
+		g.everforest_diagnostic_line_highlight = true
+		g.everforest_diagnostic_virtual_text = 'colored'
+		-- g.everforest_disable_italic_comment = true
+	end
 
 -- Set the colorscheme
-vim.cmd('colorscheme ' .. colorscheme_name)
+	vim.cmd('colorscheme ' .. colorscheme_name)
+
+end
 
