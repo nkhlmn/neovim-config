6bf1a5dc0d0003e510027a09a8564138fd48f4d4
nkhlmn <n/a>
2022-01-24 09:55:57 -0800

    Disable auto-install/update treesitter parsers
    Also disable experimental indent feature

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = 'maintained', -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  -- ensure_installed = 'maintained', -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   highlight = {
     enable = true,              -- false will disable the whole extension
   },
@@ -12,9 +12,9 @@ require'nvim-treesitter.configs'.setup {
       node_decremental = 'grm',
     },
   },
-  indent = {
-    enable = true
-  }
+  -- indent = { -- experimental; disabled for now
+  --   enable = true
+  -- }
 }
 
 vim.cmd[[
