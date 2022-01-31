a039dcc11ae45d329d8019928194a721a313bcb1
nkhl <nkhl@pop-os.localdomain>
2022-01-30 19:03:49 -0800

    Update lualine separators

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,4 +1,8 @@
 require('lualine').setup({
+    options = {
+        section_separators = { left = '', right = '' },
+        component_separators = { left = '', right = '' },
+    },
     tabline = {
         lualine_a = { 'tabs' },
         lualine_c = { 'buffers' },
