38f08ba57bc2f560d83faa42bfba14b846e09d55
nkhl <nkhl@pop-os.localdomain>
2022-02-15 00:53:27 -0800

    Add mappings for prev/next git hunks

--- after/plugin/gitsigns.lua
+++ after/plugin/gitsigns.lua
@@ -1,1 +1,4 @@
 require('gitsigns').setup()
+
+vim.api.nvim_set_keymap('n', '[g', ':Gitsigns prev_hunk<CR>', { silent = true })
+vim.api.nvim_set_keymap('n', ']g', ':Gitsigns next_hunk<CR>', { silent = true })
