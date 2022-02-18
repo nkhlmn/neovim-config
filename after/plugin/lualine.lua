f7362872c264140c5a723cd7754fcca4648fe8cd
nkhlmn <n/a>
2022-02-18 02:17:29 -0800

    Add bufferline.vim; update lualine config

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -3,9 +3,7 @@ require('lualine').setup {
     section_separators = { left = '', right = '' },
     component_separators = { left = '', right = '' },
   },
-  tabline = {
-    lualine_a = { 'tabs' },
-    lualine_c = { 'buffers' },
-    lualine_y = { require('config.utils').get_attached_lsp_servers },
+  sections = {
+    lualine_c = { require('config.utils').get_attached_lsp_servers },
   },
 }
--- lua/config/packer_config.lua
+++ lua/config/packer_config.lua
@@ -41,10 +41,10 @@ local function on_startup(use)
     'nvim-telescope/telescope-ui-select.nvim',
     'nvim-telescope/telescope-file-browser.nvim',
   }
-
   use { 'windwp/nvim-autopairs' }
   use { 'numToStr/Comment.nvim', config = function() require('Comment').setup() end }
   use { 'nvim-lualine/lualine.nvim', requires = { 'kyazdani42/nvim-web-devicons', opt = true } }
+  use { 'akinsho/bufferline.nvim', requires = 'kyazdani42/nvim-web-devicons' }
   use { 'lewis6991/gitsigns.nvim', requires = 'nvim-lua/plenary.nvim' }
   use { 'lukas-reineke/indent-blankline.nvim' }
   use { 'rcarriga/nvim-notify' }
