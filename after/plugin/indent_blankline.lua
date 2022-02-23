6dbbff4e277e206cee6037f67c55e86467249124
nkhl <nkhl@pop-os.localdomain>
2022-02-22 22:37:12 -0800

    Disable indent guide for git buffers

--- after/plugin/indent_blankline.lua
+++ after/plugin/indent_blankline.lua
@@ -1,6 +1,7 @@
 require('indent_blankline').setup {
   char = '¦',
   buftype_exclude = { 'terminal' },
+  filetype_exclude = { 'gitcommit', 'fugitive' },
   show_current_context = true,
   context_char = '¦',
 }
