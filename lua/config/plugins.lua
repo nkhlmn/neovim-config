ca3eca19f86998ab79c0d49a24c587eaa490c28e
nkhlmn <n/a>
2022-02-27 16:49:03 -0800

    Cleanup plugins.lua

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -12,68 +12,62 @@ if fn.empty(fn.glob(install_path)) > 0 then
 end
 
 local function on_startup(use)
-  use { 'wbthomason/packer.nvim' }
-
-  -- LSP
   use {
-    'neovim/nvim-lspconfig',
-    'williamboman/nvim-lsp-installer',
-    'simrat39/rust-tools.nvim',
-    'nanotee/sqls.nvim',
-  }
+    -- Packer
+    { 'wbthomason/packer.nvim' },
 
-  -- Completion
-  use {
-    'hrsh7th/nvim-cmp',
-    'hrsh7th/cmp-nvim-lsp',
-    'hrsh7th/cmp-nvim-lua',
-    'hrsh7th/cmp-buffer',
-    'hrsh7th/cmp-path',
-    'hrsh7th/cmp-calc',
-    'hrsh7th/cmp-vsnip',
-    'hrsh7th/vim-vsnip',
-    'hrsh7th/vim-vsnip-integ',
-  }
+    -- LSP
+    { 'neovim/nvim-lspconfig' },
+    { 'williamboman/nvim-lsp-installer' },
+    { 'simrat39/rust-tools.nvim' },
+    { 'nanotee/sqls.nvim' },
 
-  -- Telescope
-  use {
-    'nvim-telescope/telescope.nvim',
-    'nvim-telescope/telescope-ui-select.nvim',
-    'nvim-telescope/telescope-file-browser.nvim',
+    -- Completion
+    { 'hrsh7th/nvim-cmp' },
+    { 'hrsh7th/cmp-nvim-lsp' },
+    { 'hrsh7th/cmp-nvim-lua' },
+    { 'hrsh7th/cmp-buffer' },
+    { 'hrsh7th/cmp-path' },
+    { 'hrsh7th/cmp-calc' },
+    { 'hrsh7th/cmp-vsnip' },
+    { 'hrsh7th/vim-vsnip' },
+    { 'hrsh7th/vim-vsnip-integ' },
+
+    -- Telescope
+    { 'nvim-telescope/telescope.nvim' },
+    { 'nvim-telescope/telescope-ui-select.nvim' },
+    { 'nvim-telescope/telescope-file-browser.nvim' },
     { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' },
-  }
 
-  -- Misc
-  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
-  use { 'nvim-lualine/lualine.nvim', requires = { 'kyazdani42/nvim-web-devicons', opt = true } }
-  use { 'akinsho/bufferline.nvim', requires = 'kyazdani42/nvim-web-devicons' }
-  use { 'lewis6991/gitsigns.nvim', requires = 'nvim-lua/plenary.nvim' }
-  use { 'lukas-reineke/indent-blankline.nvim' }
-  use { 'windwp/nvim-autopairs' }
-  use { 'numToStr/Comment.nvim', config = function() require('Comment').setup() end }
-  use { 'rcarriga/nvim-notify' }
-  use { 'folke/trouble.nvim', requires = 'kyazdani42/nvim-web-devicons' }
-  use { 'mattn/emmet-vim' }
-  use { 'chrisbra/csv.vim' }
-  use { 'sbdchd/neoformat' }
-  use { 'akinsho/nvim-toggleterm.lua' }
-  use { 'kristijanhusak/vim-dadbod-ui', requires = 'tpope/vim-dadbod' }
+    -- Misc
+    { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' },
+    { 'nvim-lualine/lualine.nvim', requires = { 'kyazdani42/nvim-web-devicons', opt = true } },
+    { 'akinsho/bufferline.nvim', requires = 'kyazdani42/nvim-web-devicons' },
+    { 'lewis6991/gitsigns.nvim', requires = 'nvim-lua/plenary.nvim' },
+    { 'lukas-reineke/indent-blankline.nvim' },
+    { 'windwp/nvim-autopairs' },
+    { 'numToStr/Comment.nvim', config = function() require('Comment').setup() end, },
+    { 'rcarriga/nvim-notify' },
+    { 'folke/trouble.nvim', requires = 'kyazdani42/nvim-web-devicons' },
+    { 'mattn/emmet-vim' },
+    { 'chrisbra/csv.vim' },
+    { 'sbdchd/neoformat' },
+    { 'akinsho/nvim-toggleterm.lua' },
 
-  -- tpope
-  use {
-    'tpope/vim-fugitive',
-    'tpope/vim-repeat',
-    'tpope/vim-surround',
-    'tpope/vim-unimpaired',
-  }
+    -- tpope
+    { 'tpope/vim-fugitive' },
+    { 'tpope/vim-repeat' },
+    { 'tpope/vim-surround' },
+    { 'tpope/vim-unimpaired' },
+    { 'tpope/vim-dadbod' },
+    { 'kristijanhusak/vim-dadbod-ui', requires = 'tpope/vim-dadbod' },
 
-  -- Colorschemes
-  use {
-    'sainnhe/gruvbox-material',
-    'sainnhe/everforest',
-    'sainnhe/sonokai',
-    'ajh17/Spacegray.vim',
-    'rebelot/kanagawa.nvim',
+    -- Colorschemes
+    { 'sainnhe/gruvbox-material' },
+    { 'sainnhe/everforest' },
+    { 'sainnhe/sonokai' },
+    { 'ajh17/Spacegray.vim' },
+    { 'rebelot/kanagawa.nvim' },
   }
 
   if PACKER_BOOTSTRAP then
