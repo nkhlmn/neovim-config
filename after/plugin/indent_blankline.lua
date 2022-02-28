67a07d7e3aae3ba5a0c336aeb29bca95ed2eb881
nkhlmn <n/a>
2022-02-27 16:50:59 -0800

    Disable indentline in help files

--- after/plugin/indent_blankline.lua
+++ after/plugin/indent_blankline.lua
@@ -1,7 +1,7 @@
 require('indent_blankline').setup {
   char = '¦',
   buftype_exclude = { 'terminal' },
-  filetype_exclude = { 'gitcommit', 'fugitive' },
+  filetype_exclude = { 'gitcommit', 'fugitive', 'help' },
   show_current_context = true,
   context_char = '¦',
 }
