0565a607de104e79d11051f55b323b7c0d965f74
nkhl <nkhl@pop-os.localdomain>
2022-02-28 23:04:19 -0800

    Disable indent guides in packer filetypes

--- after/plugin/indent_blankline.lua
+++ after/plugin/indent_blankline.lua
@@ -1,7 +1,7 @@
 require('indent_blankline').setup {
   char = '¦',
   buftype_exclude = { 'terminal' },
-  filetype_exclude = { 'gitcommit', 'fugitive', 'help' },
+  filetype_exclude = { 'gitcommit', 'fugitive', 'help', 'packer' },
   show_current_context = true,
   context_char = '¦',
 }
