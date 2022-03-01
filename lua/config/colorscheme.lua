e811d740c0d61ca551d55e14acbe3275af3d61b9
nkhl <nkhl@pop-os.localdomain>
2022-02-28 23:52:59 -0800

    Improve spacegray colorscheme support

--- lua/config/colorscheme.lua
+++ lua/config/colorscheme.lua
@@ -31,7 +31,19 @@ elseif colorscheme == 'sonokai' then
   g.sonokai_diagnostic_line_highlight = true
   g.sonokai_diagnostic_virtual_text = 'colored'
   -- g.sonokai_disable_italic_comment = true
+elseif colorscheme == 'spacegray' then
+  g.spacegray_use_italics = true
+  g.spacegray_low_contrast = true
 end
 
 -- Set the colorscheme
 vim.cmd('colorscheme ' .. colorscheme)
+
+-- Customize highlights for certain themes
+if colorscheme == 'spacegray' then
+  vim.cmd [[
+  hi DiffChange ctermbg=NONE ctermfg=33  guibg=NONE guifg=#66899D cterm=NONE gui=NONE
+  hi SignColumn ctermbg=NONE ctermfg=250 guibg=NONE guifg=#B3B8C4 cterm=NONE gui=NONE
+  hi LineNr     ctermbg=NONE ctermfg=8   guibg=NONE guifg=#3E4853 cterm=NONE gui=NONE
+  ]]
+end
