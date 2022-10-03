f58e07859aa97e7f19d04faf7c3b2519699fd0be
Nikhil <nikhil.kamineni@gmail.com>
2022-10-03 19:09:06 -0400

    Minor colorscheme config cleanup

--- lua/config/colorscheme.lua
+++ lua/config/colorscheme.lua
@@ -11,27 +11,26 @@ local colorscheme = vim.g.local_colorscheme or 'gruvbox-material'
 -- Theme specific config options
 if colorscheme == 'gruvbox-material' then
   g.gruvbox_material_background = 'hard'
-  g.gruvbox_material_sign_column_background = 'none'
   g.gruvbox_material_diagnostic_text_highlight = true
   g.gruvbox_material_diagnostic_line_highlight = true
   g.gruvbox_material_diagnostic_virtual_text = 'colored'
   g.gruvbox_material_enable_bold = true
   g.gruvbox_material_better_performance = true
-  g.gruvbox_material_disable_italic_comment = true
+  -- g.gruvbox_material_disable_italic_comment = true
 elseif colorscheme == 'everforest' then
   g.everforest_background = 'hard'
-  g.everforest_sign_column_background = 'none'
   g.everforest_diagnostic_text_highlight = true
   g.everforest_diagnostic_line_highlight = true
   g.everforest_diagnostic_virtual_text = 'colored'
   g.everforest_better_performance = true
-  g.everforest_disable_italic_comment = true
+  -- g.everforest_disable_italic_comment = true
 elseif colorscheme == 'sonokai' then
   g.sonokai_style = 'shusia'
   g.sonokai_diagnostic_text_highlight = true
   g.sonokai_diagnostic_line_highlight = true
   g.sonokai_diagnostic_virtual_text = 'colored'
-  g.sonokai_disable_italic_comment = true
+  g.sonokai_better_performance = true
+  -- g.sonokai_disable_italic_comment = true
 elseif colorscheme == 'spacegray' then
   g.spacegray_use_italics = true
   g.spacegray_low_contrast = true
