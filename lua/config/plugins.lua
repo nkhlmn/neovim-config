72d92d9e95364285766bcf38990d3399308340bb
Nikhil <nikhil.kamineni@gmail.com>
2022-10-08 17:55:00 -0700

    Add spectur.nvim + format plugins.lua

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -1,18 +1,18 @@
 local fn = vim.fn
 local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
-  PACKER_BOOTSTRAP = fn.system {
+  PACKER_BOOTSTRAP = fn.system({
     'git',
     'clone',
     '--depth',
     '1',
     'https://github.com/wbthomason/packer.nvim',
     install_path,
-  }
+  })
 end
 
 local function on_startup(use)
-  use {
+  use({
     -- Packer
     { 'wbthomason/packer.nvim' },
 
@@ -44,13 +44,40 @@ local function on_startup(use)
     { 'stevearc/dressing.nvim' },
     { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' },
     { 'nvim-lualine/lualine.nvim', requires = { 'kyazdani42/nvim-web-devicons', opt = true } },
-    { 'akinsho/bufferline.nvim', requires = 'kyazdani42/nvim-web-devicons', config = function() require'bufferline'.setup() end },
+    {
+      'akinsho/bufferline.nvim',
+      requires = 'kyazdani42/nvim-web-devicons',
+      config = function()
+        require('bufferline').setup()
+      end,
+    },
     { 'lewis6991/gitsigns.nvim' },
     { 'lukas-reineke/indent-blankline.nvim' },
-    { 'windwp/nvim-autopairs', config = function() require'nvim-autopairs'.setup() end },
-    { 'kylechui/nvim-surround', config = function() require'nvim-surround'.setup() end },
-    { 'numToStr/Comment.nvim', config = function() require'Comment'.setup() end },
-    { 'folke/trouble.nvim', requires = 'kyazdani42/nvim-web-devicons', config = function() require'trouble'.setup() end },
+    {
+      'windwp/nvim-autopairs',
+      config = function()
+        require('nvim-autopairs').setup()
+      end,
+    },
+    {
+      'kylechui/nvim-surround',
+      config = function()
+        require('nvim-surround').setup()
+      end,
+    },
+    {
+      'numToStr/Comment.nvim',
+      config = function()
+        require('Comment').setup()
+      end,
+    },
+    {
+      'folke/trouble.nvim',
+      requires = 'kyazdani42/nvim-web-devicons',
+      config = function()
+        require('trouble').setup()
+      end,
+    },
     { 'rcarriga/nvim-notify' },
     { 'mattn/emmet-vim' },
     { 'chrisbra/csv.vim' },
@@ -69,12 +96,15 @@ local function on_startup(use)
     { 'sainnhe/sonokai' },
     { 'ajh17/Spacegray.vim' },
     { 'rebelot/kanagawa.nvim' },
-  }
+
+    -- Personal
+    { 'nkhlmn/spectur.nvim' },
+  })
 
   local local_plugins = vim.g.local_plugins
   if local_plugins ~= nil then
     for _, val in ipairs(local_plugins) do
-      use { val }
+      use({ val })
     end
   end
 
