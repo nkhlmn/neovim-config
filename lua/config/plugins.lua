69145ee89122e3ca0e902c2b297db66164d492c7
Nikhil <nikhil.kamineni@gmail.com>
2022-10-16 21:57:11 -0700

    Update packer startup

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -1,15 +1,15 @@
-local fn = vim.fn
-local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
-if fn.empty(fn.glob(install_path)) > 0 then
-  PACKER_BOOTSTRAP = fn.system({
-    'git',
-    'clone',
-    '--depth',
-    '1',
-    'https://github.com/wbthomason/packer.nvim',
-    install_path,
-  })
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
 end
+
+local packer_bootstrap = ensure_packer()
 
 local function on_startup(use)
   use({
@@ -123,9 +123,9 @@ local function on_startup(use)
     end
   end
 
-  if PACKER_BOOTSTRAP then
+  if packer_bootstrap then
     require('packer').sync()
   end
 end
 
-require('packer').startup(on_startup)
+return require('packer').startup(on_startup)
