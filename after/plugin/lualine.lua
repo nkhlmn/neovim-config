51d8a49a6e09af562e0412e5116b1ffe7f4d987d
Nikhil <nikhil.kamineni@gmail.com>
2022-10-28 22:34:15 -0700

    Don't hide winbar for inactive windows

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -12,4 +12,8 @@ require('lualine').setup({
     lualine_b = { require('nvim-navic').get_location },
     lualine_y = { { 'filename', path = 1 } },
   },
+  inactive_winbar = {
+    lualine_b = { require('nvim-navic').get_location },
+    lualine_y = { { 'filename', path = 1 } },
+  },
 })
