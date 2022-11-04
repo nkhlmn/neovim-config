48a2cc06aba8777ff9683fb0335a3d915afb64bb
Nikhil <nikhil.kamineni@gmail.com>
2022-11-03 22:44:44 -0700

    Refactor config.utils

--- lua/config/utils/init.lua
+++ lua/config/utils/init.lua
@@ -1,5 +1,7 @@
+local M = {}
+
 --[[ GET ATTACHED LSP SERVER NAMES ]]
-local function get_attached_lsp_servers()
+function M.get_attached_lsp_servers()
   local attached_servers = vim.lsp.get_active_clients()
   local server_names = {}
 
@@ -11,19 +13,15 @@ local function get_attached_lsp_servers()
 end
 
 --[[ TOGGLE WHITESPACE ]]
-local function toggle_whitespace()
+function M.toggle_whitespace()
   vim.o.list = not vim.o.list
 end
 
 --[[ TOGGLE DIFF ]]
-local function toggle_diff()
+function M.toggle_diff()
   local action = (vim.o.diff == true and 'off' or 'this')
   vim.cmd('windo diff' .. action)
 end
 
-return {
-  get_attached_lsp_servers = get_attached_lsp_servers,
-  toggle_whitespace = toggle_whitespace,
-  toggle_diff = toggle_diff,
-}
+return M
 
