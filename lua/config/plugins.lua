7b3835b50c6ea51d1e5d7b5461445c41a1c25ef4
Nikhil <nikhil.kamineni@gmail.com>
2022-11-16 19:11:56 -0800

    enable more null-ls features

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -30,6 +30,14 @@ local function on_startup(use)
         null_ls.setup({
           sources = {
             null_ls.builtins.diagnostics.selene,
+            null_ls.builtins.diagnostics.markdownlint,
+            null_ls.builtins.diagnostics.gitlint,
+            null_ls.builtins.diagnostics.eslint_d,
+            null_ls.builtins.diagnostics.yamllint,
+            null_ls.builtins.diagnostics.jsonlint,
+            null_ls.builtins.code_actions.eslint_d,
+            null_ls.builtins.code_actions.gitsigns,
+            null_ls.builtins.formatting.prettierd,
           },
         })
       end,
