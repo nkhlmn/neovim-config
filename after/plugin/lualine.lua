941b78c04b4bf7113f2802ffbb06194685e1aab4
Nikhil <nikhil.kamineni@gmail.com>
2023-04-02 20:00:53 -0700

    Update lualine config

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,4 +1,14 @@
 local utils = require('config.utils')
+local navic = require('nvim-navic')
+
+local navic_component = {
+  function()
+    return navic.get_location()
+  end,
+  cond = function()
+    return navic.is_available()
+  end,
+}
 
 require('lualine').setup({
   options = {
@@ -7,21 +17,25 @@ require('lualine').setup({
     globalstatus = true,
   },
   sections = {
-    lualine_a = { 'mode', utils.get_visual_lines_count },
+    lualine_a = {
+      {
+        'mode',
+        fmt = function(str)
+          return str:sub(1, 1)
+        end,
+      },
+      utils.get_visual_lines_count,
+    },
     lualine_c = {
-      'filename',
       '%=',
-      utils.get_attached_lsp_servers,
+      navic_component,
     },
     lualine_x = { 'searchcount', 'encoding', 'fileformat', 'filetype' },
   },
   winbar = {
-    lualine_b = { require('nvim-navic').get_location },
-    lualine_y = { { 'filename', path = 3 } },
+    lualine_c = { { 'filename', path = 3 } },
   },
   inactive_winbar = {
-    lualine_b = { require('nvim-navic').get_location },
-    lualine_y = { { 'filename', path = 3 } },
+    lualine_c = { { 'filename', path = 3 } },
   },
 })
-
