207e85fcb5b320e84ab2f3c96c48b26bf3578c3e
Nikhil Kamineni <nkamineni@nk-hcg-mbp.local>
2023-04-27 15:37:02 -0700

    Improve lazy.nvim initialization

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,14 +1,24 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+-- bootstrap lazy.nvim if it isn't already installed
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
     lazypath,
   })
 end
+
+-- add lazy.nvim to runtime path
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup('config.plugins', { dev = { path = '~/development' } })
+-- setup lazy.nvim
+local opts = {
+  dev = {
+    path = '~/development',
+    fallback = true
+  }
+}
+require('lazy').setup('config.plugins', opts)
