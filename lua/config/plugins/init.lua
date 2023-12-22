13f65f9465b50ff6a39c51b1565e97fe21a7fd73
Nikhil <nikhil.kamineni@gmail.com>
2023-12-22 12:49:02 -0800

    add oil.nvim

--- lua/config/plugins/init.lua
+++ lua/config/plugins/init.lua
@@ -2,6 +2,14 @@ return {
   -- Misc
   { 'chrisbra/csv.vim' },
 
+  {
+    'stevearc/oil.nvim',
+    opts = {},
+    config = function ()
+      require('oil').setup()
+    end
+  },
+
   -- Personal
   { 'nkhlmn/spectur.nvim', dev = true },
   { 'nkhlmn/gore.nvim', dev = true },
