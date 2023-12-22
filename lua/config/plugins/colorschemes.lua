945f63272c47c79ed1c38227e2982da2450c830c
Nikhil <nikhil.kamineni@gmail.com>
2023-12-22 12:50:18 -0800

    update default colorscheme

--- lua/config/plugins/colorschemes.lua
+++ lua/config/plugins/colorschemes.lua
@@ -5,16 +5,5 @@ return {
 
   { 'sainnhe/sonokai' },
 
-  {
-    'rebelot/kanagawa.nvim',
-    config = function ()
-      require('kanagawa').setup({
-        overrides = function(colors)
-          return {
-            NormalFloat = { bg = colors.palette.dragonBlack4 }
-          }
-        end,
-      })
-    end
-  },
+  { 'rebelot/kanagawa.nvim' },
 }
