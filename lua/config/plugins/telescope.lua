21e776828ed580088c5fbe62e800de7a74fdaf49
Nikhil <nikhil.kamineni@gmail.com>
2024-01-07 00:10:09 -0800

    move telescope setup

--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -2,8 +2,42 @@ return {
   {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.4',
-    dependencies = 'nvim-lua/plenary.nvim',
+    dependencies = {
+      { 'nvim-telescope/telescope-file-browser.nvim' },
+      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+      { 'nvim-lua/plenary.nvim' }
+    },
+    config = function ()
+      local telescope = require('telescope')
+      local fb_actions = require('telescope').extensions.file_browser.actions
+
+      -- Configuration
+      telescope.setup({
+        defaults = {
+          layout_config = { prompt_position = 'top' },
+          sorting_strategy = 'ascending',
+          dynamic_preview_title = true,
+        },
+        extensions = {
+          file_browser = {
+            theme = 'ivy',
+            mappings = {
+              ['n'] = {
+                ['R'] = fb_actions.rename,
+                ['%'] = fb_actions.create,
+                ['D'] = fb_actions.remove,
+                ['-'] = fb_actions.goto_parent_dir,
+                ['_'] = fb_actions.goto_cwd,
+              },
+            },
+          },
+        },
+      })
+
+      telescope.load_extension('file_browser')
+      telescope.load_extension('fzf')
+      telescope.load_extension('contemplate')
+      telescope.load_extension('gore')
+    end
   },
-  { 'nvim-telescope/telescope-file-browser.nvim' },
-  { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
 }
