4ff9365fc8247db48dec6c07e3f016711f321614
Nikhil <nikhil.kamineni@gmail.com>
2024-01-07 00:21:51 -0800

    move lualine setup

--- lua/config/plugins/ui.lua
+++ lua/config/plugins/ui.lua
@@ -31,5 +31,28 @@ return {
 
   { 'stevearc/dressing.nvim' },
 
-  { 'nvim-lualine/lualine.nvim' },
+  {
+    'nvim-lualine/lualine.nvim',
+    config = function ()
+      require('lualine').setup({
+        options = {
+          icons_enabled = false,
+          section_separators = { left = '', right = '' },
+          component_separators = { left = '', right = '' },
+          globalstatus = true,
+        },
+        winbar = {
+          lualine_c = { { 'filename', path = 3 } },
+        },
+        inactive_winbar = {
+          lualine_c = { { 'filename', path = 3 } },
+        },
+        tabline = {
+          lualine_c = { 'buffers' },
+          lualine_x = { vim.loop.cwd },
+          lualine_z = { 'tabs' }
+        }
+      })
+    end
+  },
 }
