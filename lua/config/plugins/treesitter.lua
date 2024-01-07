26551d449792d8ebc16372439430c17f17ed7754
Nikhil <nikhil.kamineni@gmail.com>
2024-01-07 00:00:37 -0800

    simplify treesitter setup

--- lua/config/plugins/treesitter.lua
+++ lua/config/plugins/treesitter.lua
@@ -1,6 +1,23 @@
 return {
-  { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
-  { 'nvim-treesitter/nvim-treesitter-context' },
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    config = function ()
+      require('nvim-treesitter.configs').setup({
+        highlight = {
+          enable = true,
+        },
+        refactor = {
+          highlight_definitions = {
+            enable = true,
+            clear_on_cursor_move = false, -- Set to false if you have an `updatetime` of ~100.
+          },
+        },
+      })
+    end
+  },
+
+  { 'nvim-treesitter/nvim-treesitter-context', config = true },
+
   { 'nvim-treesitter/nvim-treesitter-refactor' },
 }
-
