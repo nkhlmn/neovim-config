885851cf145cc1573c86399f73619f505e4d137a
Nikhil <nikhil.kamineni@gmail.com>
2024-03-25 17:01:36 -0700

    keymaps: update harpoon maps

--- lua/config/plugins/harpoon.lua
+++ lua/config/plugins/harpoon.lua
@@ -10,15 +10,12 @@ return {
     vim.keymap.set('n', '<leader>ha', function () harpoon:list():append() end)
     vim.keymap.set('n', '<leader>ho', function () harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-    vim.keymap.set('n', '<C-j>', function () harpoon:list():select(1) end)
-    vim.keymap.set('n', '<C-k>', function () harpoon:list():select(2) end)
-    vim.keymap.set('n', '<C-l>', function () harpoon:list():select(3) end)
-    vim.keymap.set('n', '<C-;>', function () harpoon:list():select(4) end)
-
     vim.keymap.set('n', '<leader>1', function () harpoon:list():select(1) end)
     vim.keymap.set('n', '<leader>2', function () harpoon:list():select(2) end)
     vim.keymap.set('n', '<leader>3', function () harpoon:list():select(3) end)
     vim.keymap.set('n', '<leader>4', function () harpoon:list():select(4) end)
+    vim.keymap.set('n', '<leader>5', function () harpoon:list():select(5) end)
+    vim.keymap.set('n', '<leader>6', function () harpoon:list():select(6) end)
 
     -- Toggle previous & next buffers stored within Harpoon list
     vim.keymap.set('n', '[h', function () harpoon:list():prev() end)
