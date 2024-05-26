b49d77f13b7c09be07ae5fead7837bda31601c65
Nikhil <nikhil.kamineni@gmail.com>
2024-05-25 22:13:33 -0700

    treesitter: enable smart rename

--- lua/config/plugins/treesitter.lua
+++ lua/config/plugins/treesitter.lua
@@ -19,5 +19,20 @@ return {
 
   { 'nvim-treesitter/nvim-treesitter-context', config = true },
 
-  { 'nvim-treesitter/nvim-treesitter-refactor' },
+  {
+    'nvim-treesitter/nvim-treesitter-refactor',
+    config = function ()
+      require 'nvim-treesitter.configs'.setup {
+        refactor = {
+          smart_rename = {
+            enable = true,
+            -- Assign keymaps to false to disable them, e.g. `smart_rename = false`.
+            keymaps = {
+              smart_rename = 'grr',
+            },
+          },
+        },
+      }
+    end
+  },
 }
