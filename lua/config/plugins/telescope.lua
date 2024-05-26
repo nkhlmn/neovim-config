c325058be652b673c627f0b13f98837e37edd47d
Nikhil <nikhil.kamineni@gmail.com>
2024-05-25 22:12:59 -0700

    telescope: upgrade to 0.1.8

--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -1,7 +1,7 @@
 return {
   {
     'nvim-telescope/telescope.nvim',
-    tag = '0.1.6',
+    tag = '0.1.8',
     dependencies = {
       { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
       { 'nvim-lua/plenary.nvim' }
