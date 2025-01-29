95935d5121e85830d0bf03b80dea7b99ddb427ee
Nikhil <nikhil.kamineni@gmail.com>
2025-01-29 11:46:09 -0800

    colorscheme: kanagawa tweaks

--- lua/config/plugins/colorschemes.lua
+++ lua/config/plugins/colorschemes.lua
@@ -2,18 +2,10 @@ return {
   {
     'rebelot/kanagawa.nvim',
     opts = {
-      colors = {
-        theme = {
-          all = {
-            ui = {
-              bg_gutter = 'none'
-            }
-          }
-        }
-      },
       overrides = function (colors)
         local theme = colors.theme
         return {
+          -- Borderless Telescope
           TelescopeTitle = { fg = theme.ui.special, bold = true },
           TelescopePromptNormal = { bg = theme.ui.bg_p1 },
           TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
@@ -21,11 +13,12 @@ return {
           TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
           TelescopePreviewNormal = { bg = theme.ui.bg_dim },
           TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
-          FloatBorder = { fg = theme.ui.bg_dim },
-          Pmenu = { bg = theme.ui.bg_p1 },
-          PmenuSel = { bg = theme.ui.bg_dim },
-          PmenuSbar = { bg = theme.ui.bg_p1 },
-          PmenuThumb = { bg = theme.ui.nontext }
+
+          -- Dark completion (popup) menu
+          Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 }, -- add `blend = vim.o.pumblend` to enable transparency
+          PmenuSel = { fg = 'NONE', bg = theme.ui.bg_p2 },
+          PmenuSbar = { bg = theme.ui.bg_m1 },
+          PmenuThumb = { bg = theme.ui.bg_p2 },
         }
       end,
     },
