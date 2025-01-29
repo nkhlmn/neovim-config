43974391a3f69c56b90a4e46d913a2dd2cc1c004
Nikhil <nikhil.kamineni@gmail.com>
2025-01-29 11:38:51 -0800

    fix: use vim.uv instead of vim.loop

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,6 +1,6 @@
 -- bootstrap lazy.nvim if it isn't already installed
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
+if not vim.uv.fs_stat(lazypath) then
   vim.fn.system({
     'git',
     'clone',
