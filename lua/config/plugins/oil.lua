0183fc2b0995872926162bafd1070c90f398533e
Nikhil <nikhil.kamineni@gmail.com>
2025-01-29 11:44:51 -0800

    oil: add keymap for `q` to close window

--- lua/config/plugins/oil.lua
+++ lua/config/plugins/oil.lua
@@ -1,22 +1,23 @@
 return {
-  {
-    'stevearc/oil.nvim',
-    opts = {
-      columns = {
-        { 'permissions', highlight = 'Debug' },
-        { 'mtime' },
-        { 'size', highlight = 'Debug' }
-      },
-      keymaps = {
-        ['<C-s>'] = false,
-        ['<C-h>'] = false,
-        ['<C-t>'] = false,
-        ['<C-p>'] = false,
-        ['<leader>v'] = 'actions.select_vsplit',
-        ['<leader>s'] = 'actions.select_split',
-        ['<leader>t'] = 'actions.select_tab',
-        ['<leader>p'] = 'actions.preview',
-      }
-    }
-  }
+	{
+		'stevearc/oil.nvim',
+		opts = {
+			columns = {
+				{ 'permissions', highlight = 'Debug' },
+				{ 'mtime' },
+				{ 'size', highlight = 'Debug' },
+			},
+			keymaps = {
+				['<C-s>'] = false,
+				['<C-h>'] = false,
+				['<C-t>'] = false,
+				['<C-p>'] = false,
+				['<leader>v'] = 'actions.select_vsplit',
+				['<leader>s'] = 'actions.select_split',
+				['<leader>t'] = 'actions.select_tab',
+				['<leader>p'] = 'actions.preview',
+				['q'] = { 'actions.close', mode = 'n' },
+			},
+		},
+	},
 }
