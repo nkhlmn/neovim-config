7fe1595c2b1bee4c7b485bf3541f54b985812834
Nikhil <nikhil.kamineni@gmail.com>
2025-01-29 11:43:41 -0800

    ui updates

--- lua/config/plugins/ui.lua
+++ lua/config/plugins/ui.lua
@@ -1,26 +1,30 @@
 return {
-  { 'stevearc/dressing.nvim' },
+	{ 'stevearc/dressing.nvim' },
 
-  {
-    'nvim-lualine/lualine.nvim',
-    opts = {
-      options = {
-        icons_enabled = false,
-        section_separators = { left = '', right = '' },
-        component_separators = { left = '', right = '' },
-        globalstatus = true,
-      },
-      winbar = {
-        lualine_c = { { 'filename', path = 3 } },
-      },
-      inactive_winbar = {
-        lualine_c = { { 'filename', path = 3 } },
-      },
-      tabline = {
-        lualine_c = { 'buffers' },
-        lualine_x = { vim.loop.cwd },
-        lualine_z = { 'tabs' }
-      },
-    },
-  },
+	{
+		'nvim-lualine/lualine.nvim',
+		opts = {
+			options = {
+				section_separators = { left = '', right = '' },
+				component_separators = { left = '', right = '' },
+				globalstatus = true,
+			},
+			sections = {
+				lualine_a = { 'mode' },
+				lualine_b = { 'branch', 'diff', 'diagnostics' },
+				lualine_c = { { 'filename', path = 3 } },
+				lualine_x = { vim.uv.cwd, 'encoding', 'fileformat', 'filetype' },
+				lualine_y = { 'progress' },
+				lualine_z = { 'location' },
+			},
+		},
+	},
+
+	{ 'akinsho/bufferline.nvim', version = '*', config = true },
+
+	{
+		'b0o/incline.nvim',
+		config = true,
+		event = 'VeryLazy',
+	},
 }
