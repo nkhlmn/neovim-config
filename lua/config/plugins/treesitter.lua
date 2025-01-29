a3c04ec5a4c8acda0428ebe1fd9bbbf7571ff2d3
Nikhil <nikhil.kamineni@gmail.com>
2025-01-29 11:40:54 -0800

    streamline treesitter config

--- lua/config/plugins/treesitter.lua
+++ lua/config/plugins/treesitter.lua
@@ -1,38 +1,29 @@
 return {
-  {
-    'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
-    config = function ()
-      require('nvim-treesitter.configs').setup({
-        highlight = {
-          enable = true,
-        },
-        refactor = {
-          highlight_definitions = {
-            enable = true,
-            clear_on_cursor_move = false, -- Set to false if you have an `updatetime` of ~100.
-          },
-        },
-      })
-    end
-  },
-
-  { 'nvim-treesitter/nvim-treesitter-context', config = true },
-
-  {
-    'nvim-treesitter/nvim-treesitter-refactor',
-    config = function ()
-      require 'nvim-treesitter.configs'.setup {
-        refactor = {
-          smart_rename = {
-            enable = true,
-            -- Assign keymaps to false to disable them, e.g. `smart_rename = false`.
-            keymaps = {
-              smart_rename = 'grr',
-            },
-          },
-        },
-      }
-    end
-  },
+	{
+		'nvim-treesitter/nvim-treesitter',
+		build = ':TSUpdate',
+		dependencies = {
+			{ 'nvim-treesitter/nvim-treesitter-context' },
+			{ 'nvim-treesitter/nvim-treesitter-refactor' },
+		},
+		config = function()
+			require('nvim-treesitter.configs').setup({
+				highlight = {
+					enable = true,
+				},
+				refactor = {
+					highlight_definitions = {
+						enable = true,
+						clear_on_cursor_move = false, -- Set to false if you have an `updatetime` of ~100.
+					},
+					smart_rename = {
+						enable = true,
+						keymaps = {
+							smart_rename = 'grr',
+						},
+					},
+				},
+			})
+		end,
+	},
 }
