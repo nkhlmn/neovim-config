3a22667f55f6d50e565bfd5c22e4a07bfb32acdf
Nikhil <nikhil.kamineni@gmail.com>
2025-03-27 13:39:23 -0700

    chore: formatting; use setup_handlers() for Mason-installed lsp's

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -1,62 +1,62 @@
-local on_attach = function (client, buf)
-  -- setup keymappings
-  local keymaps = require('config.keymaps').lsp_keymaps
-  local default_keymap_opts = { noremap = true, silent = true, buffer = true }
-  local default_keymaps = keymaps.defaults
-  for _, val in pairs(default_keymaps) do
-    vim.keymap.set(val[1], val[2], val[3], val[4] or default_keymap_opts)
-  end
+local on_attach = function(client, buf)
+	-- setup keymappings
+	local keymaps = require('config.keymaps').lsp_keymaps
+	local default_keymap_opts = { noremap = true, silent = true, buffer = true }
+	local default_keymaps = keymaps.defaults
+	for _, val in pairs(default_keymaps) do
+		vim.keymap.set(val[1], val[2], val[3], val[4] or default_keymap_opts)
+	end
 
-  local lsp_keymaps = keymaps[client.name]
-  if lsp_keymaps ~= nil then
-    for _, val in pairs(lsp_keymaps) do
-      vim.keymap.set(val[1], val[2], val[3], val[4] or default_keymap_opts)
-    end
-  end
+	local lsp_keymaps = keymaps[client.name]
+	if lsp_keymaps ~= nil then
+		for _, val in pairs(lsp_keymaps) do
+			vim.keymap.set(val[1], val[2], val[3], val[4] or default_keymap_opts)
+		end
+	end
 
-  -- client specific handling
-  if client.name == 'sqls' then
-    require('sqls').on_attach(client, buf)
-  end
+	-- client specific handling
+	if client.name == 'sqls' then
+		require('sqls').on_attach(client, buf)
+	end
 end
 
-local init = function ()
-  require('mason').setup()
-  require('mason-lspconfig').setup()
+local init = function()
+	local default_opts = {
+		on_attach = on_attach,
+		capabilities = require('blink.cmp').get_lsp_capabilities(),
+	}
 
-  local default_opts = {
-    on_attach = on_attach,
-    capabilities = require('blink.cmp').get_lsp_capabilities(),
-  }
+  -- setup lsp's installed with Mason
+	require('mason').setup()
+	require('mason-lspconfig').setup()
+	require('mason-lspconfig').setup_handlers({
+		function(server_name)
+			require('lspconfig')[server_name].setup(default_opts)
+		end,
+	})
 
-  local installed_servers = require('mason-lspconfig').get_installed_servers()
-  for _, server in pairs(installed_servers) do
-    -- default setup for all servers
-    require('lspconfig')[server].setup(default_opts)
-  end
-
-  -- ocamllsp shouldn't be installed with mason so init it here
-  require('lspconfig')['ocamllsp'].setup(default_opts)
+	-- setup lsp's NOT installed with Mason
+	require('lspconfig')['ocamllsp'].setup(default_opts)
 end
 
 return {
-  {
-    'neovim/nvim-lspconfig',
-    dependencies = {
-      { 'williamboman/mason.nvim', build = ':MasonUpdate' },
-      { 'williamboman/mason-lspconfig.nvim' },
-      { 'saghen/blink.cmp' },
-    },
-    config = init
-  },
+	{
+		'neovim/nvim-lspconfig',
+		dependencies = {
+			{ 'williamboman/mason.nvim', build = ':MasonUpdate' },
+			{ 'williamboman/mason-lspconfig.nvim' },
+			{ 'saghen/blink.cmp' },
+		},
+		config = init,
+	},
 
-  {
-    'folke/lazydev.nvim',
-    ft = 'lua', -- only load on lua files
-    opts = {
-      library = {
-        { path = '${3rd}/luv/library', words = { 'vim%.uv' } }, -- Load luvit types when the `vim.uv` word is found
-      },
-    },
-  },
+	{
+		'folke/lazydev.nvim',
+		ft = 'lua', -- only load on lua files
+		opts = {
+			library = {
+				{ path = '${3rd}/luv/library', words = { 'vim%.uv' } }, -- Load luvit types when the `vim.uv` word is found
+			},
+		},
+	},
 }
