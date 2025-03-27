0f190ebe6e035a8cd28cd4980d209390c970b217
Nikhil <nikhil.kamineni@gmail.com>
2025-03-27 13:43:19 -0700

    0.11: formatting; remove/update some keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,112 +2,110 @@ local utils = require('config.utils')
 
 -- Define global keymaps
 local keymaps = {
-  -- misc
-  { '', '<leader>`', ':tabnew $MYVIMRC<CR>' }, -- open init.lua
-  { 'n', '<leader>xx', ':w|so%<CR>' }, -- source current file
-  { 'n', '<F5>', ':!open %<CR>' }, -- Open current file with default program
-  { 'n', '<leader>f', require('conform').format },
-  { 'n', '\\d', vim.diagnostic.open_float },
+	-- misc
+	{ '', '<leader>`', ':tabnew $MYVIMRC<CR>' }, -- open init.lua
+	{ 'n', '<leader>xx', ':w|so%<CR>' }, -- source current file
+	{ 'n', '<F5>', ':!open %<CR>' }, -- Open current file with default program
+	{ 'n', '<leader>f', require('conform').format },
+	{ 'n', '\\d', vim.diagnostic.open_float },
 
-  -- searching
-  { 'n', '*', '*N', {} }, -- keep cursor on first match when searching for word under cursor
-  { 'n', '<ESC><ESC>', ':let @/ = ""<cr>' }, -- clear search by pressing esc twice
+	-- searching
+	{ 'n', '*', '*N', {} }, -- keep cursor on first match when searching for word under cursor
+	{ 'n', '<ESC><ESC>', ':let @/ = ""<cr>' }, -- clear search by pressing esc twice
 
-  -- splits
-  { 'n', '<leader>s', ':new<CR>' }, -- horizontal split
-  { 'n', '<leader>v', ':vnew<CR>' }, -- vertical split
+	-- splits
+	{ 'n', '<leader>s', ':new<CR>' }, -- horizontal split
+	{ 'n', '<leader>v', ':vnew<CR>' }, -- vertical split
 
-  -- buffers
-  { 'n', '<C-s>', ':w<cr>' }, -- write/save buffer
-  { 'n', 'ZB', '<cmd>bd!<cr>' }, -- close buffer
-  { 'n', '<leader>b', ':enew<CR>' }, -- open new buffer
+	-- buffers
+	{ 'n', '<C-s>', ':w<cr>' }, -- write/save buffer
+	{ 'n', 'ZB', '<cmd>bd!<cr>' }, -- close buffer
+	{ 'n', '<leader>b', ':enew<CR>' }, -- open new buffer
 
-  -- tabs
-  { 'n', '<leader>t', ':tabnew<CR>' }, -- open new tab
-  { 'n', '[t', ':tabprevious<CR>' }, -- previous tab
-  { 'n', ']t', ':tabnext<CR>' }, -- next tab
-  { 'n', '[T', ':execute "silent! tabmove " . (tabpagenr()-2)<CR>' }, -- move tab left
-  { 'n', ']T', ':execute "silent! tabmove " . (tabpagenr()+1)<CR>' }, -- move tab right
+	-- tabs
+	{ 'n', '<leader>t', ':tabnew<CR>' }, -- open new tab
+	{ 'n', '[t', ':tabprevious<CR>' }, -- previous tab
+	{ 'n', ']t', ':tabnext<CR>' }, -- next tab
+	{ 'n', '[T', ':execute "silent! tabmove " . (tabpagenr()-2)<CR>' }, -- move tab left
+	{ 'n', ']T', ':execute "silent! tabmove " . (tabpagenr()+1)<CR>' }, -- move tab right
 
-  -- yanking
-  { 'n', '<leader>y', ':%y<CR>', {} }, -- yank entire buffer
-  { 'n', 'Y', 'y$' }, -- Yank till end of line with Y (instead of yanking entire line)
-  { 'n', 'y^', 'mzy^`z' }, -- Leave cursor where it is after yanking
-  { 'n', 'ygg', 'mzygg`z' }, -- Leave cursor where it is after yanking
-  { 'n', ',p', '"0p', { silent = true } }, -- Paste (after cursor) last thing yanked
-  { 'n', ',P', '"0P', { silent = true } }, -- Paste (before cursor) last thing yanked
+	-- yanking
+	{ 'n', '<leader>y', ':%y<CR>', {} }, -- yank entire buffer
+	{ 'n', 'Y', 'y$' }, -- Yank till end of line with Y (instead of yanking entire line)
+	{ 'n', 'y^', 'mzy^`z' }, -- Leave cursor where it is after yanking
+	{ 'n', 'ygg', 'mzygg`z' }, -- Leave cursor where it is after yanking
+	{ 'n', ',p', '"0p', { silent = true } }, -- Paste (after cursor) last thing yanked
+	{ 'n', ',P', '"0P', { silent = true } }, -- Paste (before cursor) last thing yanked
 
-  -- macros
-  { 'n', 'Q', '@@j', {} }, -- yank entire buffer
-  { 'x', 'Q', ':norm @@<CR>', {} }, -- run last run macro on all visually selected lines
+	-- macros
+	{ 'n', 'Q', '@@j', {} }, -- yank entire buffer
+	{ 'x', 'Q', ':norm @@<CR>', {} }, -- run last run macro on all visually selected lines
 
-  -- Create undo break points when hitting certain characters in insert mode (allows for more granular undos)
-  { 'i', ',', ',<c-g>u' },
-  { 'i', '.', '.<c-g>u' },
-  { 'i', '!', '!<c-g>u' },
-  { 'i', '?', '?<c-g>u' },
+	-- Create undo break points when hitting certain characters in insert mode (allows for more granular undos)
+	{ 'i', ',', ',<c-g>u' },
+	{ 'i', '.', '.<c-g>u' },
+	{ 'i', '!', '!<c-g>u' },
+	{ 'i', '?', '?<c-g>u' },
 
-  -- Custom functions
-  { 'n', '\\D', utils.toggle_diff },
-  { 'n', '\\w', utils.toggle_whitespace },
+	-- Custom functions
+	{ 'n', '\\D', utils.toggle_diff },
+	{ 'n', '\\w', utils.toggle_whitespace },
 
-  -- plugins
-  -- oil
-  { 'n', '-', '<CMD>Oil<CR>', { desc = "Open parent directory" } },
+	-- plugins
+	-- oil
+	{ 'n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' } },
 
-  ---- gitsigns
-  { 'n', '[g', ':Gitsigns prev_hunk<CR>' },
-  { 'n', ']g', ':Gitsigns next_hunk<CR>' },
+	---- gitsigns
+	{ 'n', '[g', ':Gitsigns nav_hunk prev<CR>' },
+	{ 'n', ']g', ':Gitsigns nav_hunk next<CR>' },
 
-  ---- telescope (global)
-  { 'n', '<C-t><C-t>', ':Telescope<cr>' },
-  { 'n', '<leader>p', utils.open_telescope_find_files_with_hidden },
-  { 'n', '<C-g>', require('telescope.builtin').git_status },
-  { 'n', '<C-b>', require('telescope.builtin').buffers },
-  { 'n', '<C-p>', require('telescope.builtin').find_files },
-  { 'n', '<C-f>', require('telescope.builtin').live_grep },
-  { 'n', '<C-h>', require('telescope.builtin').help_tags },
-  { 'n', '<leader>ok', require('telescope.builtin').keymaps },
-  { 'n', '<leader>oc', require('telescope.builtin').colorscheme },
-  { 'n', '<leader>ol', require('telescope.builtin').loclist },
-  { 'n', '<leader>oq', require('telescope.builtin').quickfix },
-  { 'n', '<leader>oo', require('telescope.builtin').vim_options },
-  { 'n', '<leader>oh', require('telescope.builtin').highlights },
-  { 'n', '<leader>od', require('telescope.builtin').diagnostics },
-  { 'n', '<leader>of', require('telescope.builtin').grep_string },
-  { 'n', '<leader>o;', require('telescope.builtin').command_history },
-  { 'n', '<leader>os', require('telescope.builtin').search_history },
+	---- telescope (global)
+	{ 'n', '<C-t><C-t>', ':Telescope<cr>' },
+	{ 'n', '<leader>p', utils.open_telescope_find_files_with_hidden },
+	{ 'n', '<C-g>', require('telescope.builtin').git_status },
+	{ 'n', '<C-b>', require('telescope.builtin').buffers },
+	{ 'n', '<C-p>', require('telescope.builtin').find_files },
+	{ 'n', '<C-f>', require('telescope.builtin').live_grep },
+	{ 'n', '<C-h>', require('telescope.builtin').help_tags },
+	{ 'n', '<leader>ok', require('telescope.builtin').keymaps },
+	{ 'n', '<leader>oc', require('telescope.builtin').colorscheme },
+	{ 'n', '<leader>ol', require('telescope.builtin').loclist },
+	{ 'n', '<leader>oq', require('telescope.builtin').quickfix },
+	{ 'n', '<leader>oo', require('telescope.builtin').vim_options },
+	{ 'n', '<leader>oh', require('telescope.builtin').highlights },
+	{ 'n', '<leader>od', require('telescope.builtin').diagnostics },
+	{ 'n', '<leader>of', require('telescope.builtin').grep_string },
+	{ 'n', '<leader>o;', require('telescope.builtin').command_history },
+	{ 'n', '<leader>os', require('telescope.builtin').search_history },
 }
 
 -- Set global keymaps
 local default_options = { noremap = true, silent = true }
 for _, val in pairs(keymaps) do
-  local success, _ = pcall(vim.keymap.set, val[1], val[2], val[3], val[4] or default_options)
-  if not success then
-    vim.notify("Error setting keymaps: " .. vim.inspect(val), vim.log.levels.ERROR)
-  end
+	local success, _ = pcall(vim.keymap.set, val[1], val[2], val[3], val[4] or default_options)
+	if not success then
+		vim.notify('Error setting keymaps: ' .. vim.inspect(val), vim.log.levels.ERROR)
+	end
 end
 
 -- Define lsp keymaps (set in lsp on_attach function)
 local lsp_keymaps = {
-  defaults = {
-    { 'n', 'gD', vim.lsp.buf.declaration },
-    { 'n', 'gd', vim.lsp.buf.definition },
-    { 'n', 'gs', vim.lsp.buf.signature_help },
-    { 'n', 'gr', vim.lsp.buf.references },
-    { 'n', '<leader>rn', vim.lsp.buf.rename },
-    { 'n', '<leader>ca', vim.lsp.buf.code_action },
+	defaults = {
+		-- { 'n', 'gD', vim.lsp.buf.declaration },
+		-- { 'n', 'gd', vim.lsp.buf.definition },
 
-    -- telescope (lsp)
-    { 'n', '<leader>or', require('telescope.builtin').lsp_references },
-    { 'n', '<leader>oi', require('telescope.builtin').lsp_implementations },
+		-- telescope (lsp)
+		{ 'n', '<leader>or', require('telescope.builtin').lsp_references },
+		{ 'n', '<leader>oi', require('telescope.builtin').lsp_implementations },
+		{ 'n', 'gd', require('telescope.builtin').lsp_definitions },
+		{ 'n', 'gD', require('telescope.builtin').lsp_type_definitions },
 
-    -- misc
-    { 'n', '<leader>d', require('config.utils').toggle_diagnostics },
-    { 'n', '\\h', utils.toggle_inlay_hints },
-  },
+		-- misc
+		{ 'n', '<leader>d', utils.toggle_diagnostics },
+		{ 'n', '\\h', utils.toggle_inlay_hints },
+	},
 }
 
 return {
-  lsp_keymaps = lsp_keymaps,
+	lsp_keymaps = lsp_keymaps,
 }
