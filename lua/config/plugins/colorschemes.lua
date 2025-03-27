7324654a0a08a86964396efe2751b4aa2d4cf506
Nikhil <nikhil.kamineni@gmail.com>
2025-03-27 13:46:49 -0700

    colorschemes: simplify kanagawa config

--- lua/config/plugins/colorschemes.lua
+++ lua/config/plugins/colorschemes.lua
@@ -13,12 +13,6 @@ return {
           TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
           TelescopePreviewNormal = { bg = theme.ui.bg_dim },
           TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
-
-          -- Dark completion (popup) menu
-          Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 }, -- add `blend = vim.o.pumblend` to enable transparency
-          PmenuSel = { fg = 'NONE', bg = theme.ui.bg_p2 },
-          PmenuSbar = { bg = theme.ui.bg_m1 },
-          PmenuThumb = { bg = theme.ui.bg_p2 },
         }
       end,
     },
